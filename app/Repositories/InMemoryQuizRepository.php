ff8d8c031225d361c15c4cecf8bd026dbcb4a41f
LAWRENCE PHUKA CHIKAPA <lawreay1@gmail.com>
2026-09-23 14:03:45 +0200

    feat: add in-memory quiz test doubles

---
+++ app/Repositories/InMemoryQuizRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+final class InMemoryQuizRepository implements QuizRepositoryInterface
+{
+    private array $quizzes = [];
+    private int $nextId = 1;
+
+    public function create(array $quiz): array
+    {
+        $quiz['id'] = $this->nextId++;
+        $this->quizzes[$quiz['id']] = $quiz;
+        return $quiz;
+    }
+
+    public function findById(int $id): ?array
+    {
+        return $this->quizzes[$id] ?? null;
+    }
+
+    public function findByCourse(int $courseId): array
+    {
+        return array_values(array_filter($this->quizzes, fn(array $quiz): bool => (int) $quiz['course_id'] === $courseId));
+    }
+}
