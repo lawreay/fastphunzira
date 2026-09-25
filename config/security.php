706b0a652f5ca5a34fc41ab44f8d4e21f0c62e07
LAWRENCE PHUKA CHIKAPA <lawreay1@gmail.com>
2026-09-25 18:06:49 +0200

    config: add session idle timeout

--- config/security.php
+++ config/security.php
@@ -2,6 +2,7 @@
 
 return [
     'session_lifetime' => (int) (getenv('SESSION_LIFETIME') ?: 1800),
+    'session_idle_timeout' => (int) (getenv('SESSION_IDLE_TIMEOUT') ?: 1800),
     'session_name' => getenv('SESSION_NAME') ?: 'fastphunzira_session',
     'csrf_token_name' => '_token',
     'password_algo' => PASSWORD_DEFAULT,
