50635c559055662c9ebbceccaa4dd3b69b91f0ba
LAWRENCE PHUKA CHIKAPA <lawreay1@gmail.com>
2026-09-25 19:44:44 +0200

    Configure login security thresholds

--- config/security.php
+++ config/security.php
@@ -9,4 +9,11 @@ return [
     'require_https' => filter_var(getenv('REQUIRE_HTTPS') ?: ((getenv('APP_ENV') ?: 'local') === 'production' ? 'true' : 'false'), FILTER_VALIDATE_BOOLEAN),
     'cookie_httponly' => true,
     'cookie_samesite' => 'Lax',
+
+    'login_security' => [
+        'window_seconds' => (int) (getenv('LOGIN_SECURITY_WINDOW') ?: 900),
+        'max_failures_per_account' => (int) (getenv('LOGIN_MAX_FAILURES_PER_ACCOUNT') ?: 5),
+        'max_failures_per_ip' => (int) (getenv('LOGIN_MAX_FAILURES_PER_IP') ?: 20),
+        'throttle_seconds' => (int) (getenv('LOGIN_THROTTLE_SECONDS') ?: 900),
+    ],
 ];
